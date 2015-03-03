f737daced625f2b28764edddfd4965d505f5f82c
qheuestc <qheuestc@gmail.com>
2015-03-03 20:03:32 +0800

    complete file transfer

--- app/src/main/java/com/qhe/pcontroldroid/fragments/FileTransferFragment.java
+++ app/src/main/java/com/qhe/pcontroldroid/fragments/FileTransferFragment.java
@@ -1,7 +1,11 @@
 package com.qhe.pcontroldroid.fragments;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -24,6 +28,9 @@ import org.json.JSONException;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -39,6 +46,7 @@ public class FileTransferFragment extends ListFragment {
     private String mRequestDir;
     public static final String EXTRA_REQUEST_DIR = "com.qhe.pcontroldroid.fragments.FileTransferFragment.req_dir";
     private static final String TAG = "FileTransferFragment";
+    private ProgressDialog mProgressDialog;
 
     private FragmentManager mFragmentManager;
     private RemoteFileAdapter adapter;
@@ -82,12 +90,51 @@ public class FileTransferFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         mFragmentManager = getActivity().getSupportFragmentManager();
-        RemoteFile file = ((RemoteFileAdapter)getListAdapter()).getItem(position);
-//        Toast.makeText(getActivity(), file.getAbsPath(), Toast.LENGTH_SHORT).show();
-        FileTransferFragment fragment =  FileTransferFragment.newInstance(file.getAbsPath());
-        mFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(file.getAbsPath()).commit();
-//        mFragmentManager.beginTransaction().hide(this).commit();
+        final RemoteFile file = ((RemoteFileAdapter)getListAdapter()).getItem(position);
+
+        /**
+         * 根据是否是文件夹进行相应的操作
+         * 1、如果是文件，弹出一个对话框询问用户的操作：远程打开，传输到本地
+         * 2、如果是文件夹就新开一个Fragment
+         */
+
+        if(file.getFileType().equals("folder")) {
+            FileTransferFragment fragment = FileTransferFragment.newInstance(file.getAbsPath());
+            mFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(file.getAbsPath()).commit();
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("文件选项").setMessage("文件： " + file.getFileName() + "\n" + "大小： " + FileUtils.formatFileSize(file.getFileSize()));
+            builder.setNegativeButton("取消", null);
+            builder.setNeutralButton("远程打开", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    new TransferFileTask().execute("OPEN%$%" + file.getAbsPath());
+                }
+            });
+            builder.setPositiveButton("传输到本地", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    /**
+                     * 弹出一个进度条对话框，显示文件传输的进度
+                     */
+                    mProgressDialog = new ProgressDialog(getActivity());
+                    mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                    mProgressDialog.setTitle("文件传输中...");
+                    mProgressDialog.setIcon(R.drawable.icon_file);
+                    mProgressDialog.setMessage("文件： " + file.getFileName() + "\n" + "大小： " + FileUtils.formatFileSize(file.getFileSize()));
+                    mProgressDialog.setProgress(0);
+                    mProgressDialog.setMax(100);
+                    mProgressDialog.setCancelable(false);
+                    mProgressDialog.setCanceledOnTouchOutside(false);
+                    mProgressDialog.show();
+                    new TransferFileTask().execute("TRANS%$%" + file.getAbsPath(), file.getFileSize() + "");
+                }
+            });
+            builder.show();
+        }
     }
+
+
 
     private class RemoteFileAdapter extends ArrayAdapter<RemoteFile> {
         public RemoteFileAdapter(ArrayList<RemoteFile> files) {
@@ -127,6 +174,9 @@ public class FileTransferFragment extends ListFragment {
     }
 
 
+    /**
+     * 获取计算机上的目录以及文件的信息
+     */
     private class FetchRemoteFileListTask extends AsyncTask<String, Void, ArrayList<RemoteFile>> {
         @Override
         protected void onPreExecute() {
@@ -162,7 +212,6 @@ public class FileTransferFragment extends ListFragment {
 
             if(jsonString != null) {
                 try {
-                    RemoteFile remoteFile;
                     array = (JSONArray) new JSONTokener(jsonString.toString()).nextValue();
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -178,10 +227,6 @@ public class FileTransferFragment extends ListFragment {
 
         @Override
         protected void onPostExecute(ArrayList<RemoteFile> aVoid) {
-//            for(int i=0; i<aVoid.size(); i++) {
-//                mFiles.add(aVoid.get(i));
-//            }
-
             Object[] remoteFileObj = aVoid.toArray();
             Arrays.sort(remoteFileObj);
 
@@ -190,6 +235,85 @@ public class FileTransferFragment extends ListFragment {
             }
 
             adapter.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * 对相应的点击事件进行处理
+     * 1、如果用户点击的是“远程打开”，发送远程打开的命令
+     * 2、如果用户点击的是“传输到本地”，发送传输文件的命令
+     */
+    private class TransferFileTask extends AsyncTask<String, Integer, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            File mSDCard = Environment.getExternalStorageDirectory();
+            mSDCard =  new File(mSDCard, "/PControlDroid");
+            OutputStream out = null;
+            FileOutputStream fos = null;
+            DataInputStream dis = null;
+            byte[] buffer = new byte[1024];
+            int length = -1;
+
+            Socket socket = null;
+
+            if(!mSDCard.exists()) {
+                mSDCard.mkdir();
+            }
+
+            try {
+                socket = new Socket(ConnectionManager.get(getActivity()).getServerIP(), 2016);
+                out = socket.getOutputStream();
+                out.write(params[0].getBytes());
+                long count = 0;
+                if(params[0].startsWith("TRANS")) {
+                    long fileSize = Long.parseLong(params[1]);
+                    dis = new DataInputStream(socket.getInputStream());
+                    mSDCard = new File(mSDCard, params[0].substring(params[0].lastIndexOf('/')));
+                    fos = new FileOutputStream(mSDCard);
+                    while((length = dis.read(buffer, 0, buffer.length)) > 0) {
+                        count += length;
+                        fos.write(buffer, 0, length);
+                        fos.flush();
+                        publishProgress((int) ((count / (float)fileSize) * 100));
+                    }
+                }
+
+            } catch (Exception e) {
+                Log.i(TAG, "文件传输错误: " + e);
+            } finally {
+                try {
+                    if (out != null)
+                        out.close();
+                    if(fos != null)
+                        fos.close();
+                    if(dis != null)
+                        dis.close();
+                    if(socket != null)
+                        socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            if(mProgressDialog != null)
+                mProgressDialog.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if(mProgressDialog != null)
+                mProgressDialog.cancel();
         }
     }
 }
